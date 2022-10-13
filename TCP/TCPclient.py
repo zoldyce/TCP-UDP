328dedf9d487d374a13ef27b843ef722ae4ec60c
zoldyce <boonyarit260842@gmail.com>
2022-10-13 23:07:07 +0700

    first commit

---
+++ TCP/TCPclient.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = 'LAPTOP-KJ3KB4S4'
+serverPort = 12000
+clientSocket = socket(AF_INET,SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode('utf-8'))
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ',modifiedSentence.decode('utf-8'))
+clientSocket.close()
---
+++ TCP/TPCserver.py
@@ -0,0 +1,13 @@
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(("",serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode('utf-8')
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode("utf-8"))
+    print('ResultTCP: ',capitalizedSentence)
+    connectionSocket.close()
